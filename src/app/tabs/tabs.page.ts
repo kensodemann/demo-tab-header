7fd3b3c622d80aee44815245f4c18c7f9db64ad6
Ken Sodemann <ken@ionic.io>
2021-07-09 14:20:39 -0500

    add a header to the tab

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,10 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  title = 'One';
 
   constructor() {}
 
+  change(evt: { tab: string }) {
+    switch (evt.tab) {
+      case 'tab1':
+        this.title = 'One';
+        break;
+      case 'tab2':
+        this.title = 'Two';
+        break;
+      case 'tab3':
+        this.title = 'Three';
+        break;
+
+      default:
+        this.title = 'Not sure...';
+    }
+  }
 }
